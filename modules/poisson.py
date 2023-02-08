0ae4c9ea938daa4118e37fc65d265d6ca70a0e0d
Nate Johnson <jax523@gmail.com>
2023-02-08 13:36:40 -0300

    Update poisson.py
    now uses extension parameter (> lambda)

--- modules/poisson.py
+++ modules/poisson.py
@@ -4,7 +4,7 @@ import sys
 import click
 from pathlib import Path
 from os.path import isfile, isdir
-from collections import Counter#, deque
+from collections import Counter, deque
 from pprint import pprint
 
 # import numpy as np
@@ -45,15 +45,19 @@ def poisson(alignment_file, annotation_readgroups, gene_annotation, output_direc
 	output_directory = output_directory.rstrip("/")
 
 	window = 40
+	cl_i = 0
+	merge_dist = 200
+	pad = 20
+
 	half_window = int(window/2)
 
 	chromosomes, bam_rgs = get_chromosomes(alignment_file, output_directory)
 	annotation_readgroups = check_rgs(annotation_readgroups, bam_rgs)
 
 
-	cl_i = 0
-	merge_dist = 200
-	pad = 20
+	def calc_poisson(k, l):
+		p = l**k * math.e**(-l) / math.factorial(k)
+		return(p)
 
 
 	gene_d = {}
@@ -74,12 +78,17 @@ def poisson(alignment_file, annotation_readgroups, gene_annotation, output_direc
 					gene_d[chrom] = [(start, stop)]
 
 
+	output_file = f"{output_directory}/Poisson.gff3"
 
-	with open(f"{output_directory}/poisson.gff3", 'w') as outf:
+	with open(output_file, 'w') as outf:
 		print("##gff-version 3", file=outf)
 
 		for chrom, chrom_length in chromosomes:
 			print(f"##sequence-region   {chrom} 1 {chrom_length}", file=outf)
+
+
+	loci = []
+
 
 	for chrom_count, chrom in enumerate(chromosomes):
 		chrom, chrom_length = chrom
@@ -90,11 +99,9 @@ def poisson(alignment_file, annotation_readgroups, gene_annotation, output_direc
 		print(f"chrom: {chrom}")
 		print(f"       {chrom_length:,} bp")
 
-		sam_iter = samtools_view(alignment_file, rgs=annotation_readgroups, locus=chrom)
-
 
 		depth_c  = Counter()
-		# strand_c = Counter()
+		detail_d = dict()
 
  		## Defining parts of the genome which are not from annotated mRNAs
 
@@ -109,11 +116,13 @@ def poisson(alignment_file, annotation_readgroups, gene_annotation, output_direc
 			last_stop = stop
 
 
-
 		## Iterating through reads for a chromosome and counting window depths
 
 		# read_count = 0
+
+		sam_iter = samtools_view(alignment_file, rgs=annotation_readgroups, locus=chrom)
 		for i, read in enumerate(sam_iter):
+
 
 			if (i+1) % 10000 == 0:
 				print(".", end='', flush=True)
@@ -124,6 +133,16 @@ def poisson(alignment_file, annotation_readgroups, gene_annotation, output_direc
 						print(flush=True)
 			# read_count += 1
 			sam_strand, sam_length, sam_size, sam_pos, sam_chrom, sam_rg, sam_read, sam_name = read
+
+			key = sam_pos + int(sam_length/2)
+			try:
+				detail_d[key][sam_length] += 1
+				detail_d[key][sam_strand] += 1
+			except KeyError:
+				detail_d[key] = Counter()
+				detail_d[key][sam_length] += 1
+				detail_d[key][sam_strand] += 1
+
 
 			added_to_intergenic = False
 			for r in range(sam_pos, sam_pos + sam_length + window):
@@ -139,131 +158,175 @@ def poisson(alignment_file, annotation_readgroups, gene_annotation, output_direc
 
 
 		lambda_chrom = window / intergenic_length * intergenic_read_count
+
+		lambda_chrom_rpm = lambda_chrom / i * 1000000
+
 		# lambda_chrom = window / chrom_length * read_count
 
 		print("      ", round(lambda_chrom,4), 'lambda (chromosome, intergenic)')
 
-		def poisson(k, l):
-			p = l**k * math.e**(-l) / math.factorial(k)
-			return(p)
+		print("      ", round(lambda_chrom_rpm,4), 'lambda_rpm')
+
 
 		poisson_d = {}
 		k = 0
-		threshold = 0.00001
+		# threshold = 0.00001
 		while True:
 			k += 1
 			try:
-				p = poisson(k,lambda_chrom)
+				p = calc_poisson(k,lambda_chrom)
 			except OverflowError:
 				break
 			poisson_d[k] = p
 
 
 
-		# pprint(poisson_d)
 
-
-
+		loci = deque()
+		cl_i = 0
 
 
 		in_locus = False
+		nucleated = False
 		for r in range(chrom_length):
+
 			k = depth_c[r]
-			# print(k)
+
 			try:
 				p = poisson_d[k]
 			except KeyError:
 				p = 0.0
-			# print(r, k, round(p,5), sep='\t')
 
-			if p <= 0.0001 and k > lambda_chrom:
+
+
+			if k > lambda_chrom:
+				if p <= 0.00001:
+					nucleated = True
+
 				if not in_locus:
 					start = r - pad
+					region_c = Counter()
+
+
 				in_locus = True
-				stop = r + pad 
+				stop = r + pad
+
+				try:
+					region_c.update(detail_d[r])
+				except KeyError:
+					pass
+
+
+
+
 
 
 			else:
-				if in_locus:
-					if r > stop + merge_dist:
 
+				if in_locus and nucleated:
+					if r > stop + merge_dist:
 						cl_i += 1
 
-						with open("test.gff3", 'a') as outf:
-							print(chrom, 'poissonLocus','nc_RNA', start, stop, f'.\t.\t.\tID=Cl_{cl_i}', sep='\t', file=outf)
 
+
+
+						# region_length = stop - start
+
+						# frac_top = region_c['+'] / (region_c['-'] + region_c['+'])
+
+						# total = sum([region_c[l] for l in range(15,31)])
+
+						# size_profile = [
+						# 	sum([region_c[l] for l in range(15,20)])/total,
+						# 	sum([region_c[l] for l in range(20,26)])/total,
+						# 	sum([region_c[l] for l in range(26,31)])/total
+						# ]
+
+
+						locus_name = f"Cl_{cl_i}"
+
+						with open(output_file, 'a') as outf:
+							print(chrom, 'poissonLocus','nc_RNA', start, stop, f'.\t.\t.\tID={locus_name}', sep='\t', file=outf)
+
+
+						loci.append((locus_name, chrom, start, stop))#, frac_top, size_profile, total/region_length))
 						in_locus = False
+						nucleated = False
+				else:
+					in_locus = False
+					nucleated = False
 
 
 
-				# NC_037310.1	smoothLocus	nc_RNA	360214	360303	.	-	.	ID=Cl_1;dicercall=21;frac_dicercall=0.674
+		# def calc_distance(r1, r2):
+		# 	strand_distance = abs(r1[4] - r2[4])
+		# 	length_distance = [abs(i-j) for i,j in zip(r1[5],r2[5])]
+		# 	abd_distance = r1[6] / r2[6]
+
+		# 	if abd_distance > 1:
+		# 		abd_distance = 1 / abd_distance
+
+
+		# 	return(strand_distance, length_distance, abd_distance)
+
+		# def save_locus(locus, locus_name):
+
+
+		# 	start = locus[0][2]  - pad
+		# 	stop  = locus[-1][3] + pad
+		# 	chrom = locus[0][1]
+
+		# 	with open(output_file, 'a') as outf:
+		# 		print(chrom, 'poissonLocus','nc_RNA', start, stop, f'.\t.\t.\tID={locus_name}', sep='\t', file=outf)
+
+		# 	# pprint(locus)
+		# 	# sys.exit()
+		# 	pass
+
+
+		# loci = []
+
+		# locus = [regions.popleft()]
+
+		# for region in regions:
+
+		# 	if region[2] - locus[-1][3] < 400:
+
+		# 		length_distance, strand_distance, abd_distance = calc_distance(locus[-1], region)
+
+		# 		# print(length_distance, strand_distance, abd_distance)
+
+		# 		length_pass = length_distance < 0.4
+		# 		strand_pass = sum(strand_distance)/3 < 0.4
+		# 		abd_pass    = abd_distance > 0.2
+
+		# 		# pprint(locus)
+		# 		# print(length_pass, strand_pass, abd_pass)
+
+		# 		if length_pass and strand_pass and abd_pass:
+		# 			locus.append(region)
+
+		# 		else:
+		# 			# pprint(locus)
+		# 			# print(region)
+		# 			cl_i += 1
+		# 			save_locus(locus, f"Cl_{cl_i}")
+		# 			locus = [region]
+		# 			sys.exit()
+
+		# 	else:
+		# 		cl_i += 1
+		# 		save_locus(locus, f"Cl_{cl_i}")
+		# 		locus = [region]
 
 
 
 
+			# sys.exit()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-		# depth_c = Counter()
-		# for read in sam_iter:
-
-		# 	# print(read)
-		# 	sam_strand, sam_length, sam_size, sam_pos, sam_chrom, sam_rg, sam_read = read
-
-
-		# 	for r in range(sam_length):
-		# 		depth_c[r + sam_pos] += 1
-
-
-
-		# # for r in range(chrom_length):
-		# # 	print(depth_c[r])
-
-		# 	# if sam_pos > 200:
-		# 	# 	pprint(depth_c)
-		# 	# 	sys.exit()
-
-
-		# gene_depths = []
-		# for start, stop in gene_d[chrom]:
-		# 	gene_depths += [depth_c[r] for r in range(start, stop+1)]
-
-		# intergenic_depths = []
-		# intergenic_regions = []
-		# last_stop = 0
-		# for start, stop in gene_d[chrom]:
-		# 	intergenic_regions.append((last_stop, start))
-		# 	last_stop = stop
-
-		# intergenic_regions.append((last_stop, chrom_length))
-
-
-		# for start, stop in intergenic_regions:
-		# 	intergenic_depths += [depth_c[r] for r in range(start, stop+1)]
-
-
-		# n_quantiles=100
-		# q_gene = quantiles(gene_depths, n=n_quantiles)
-		# q_inter = quantiles(intergenic_depths, n=n_quantiles)
-		# q_all  = quantiles([depth_c[r] for r in range(chrom_length)], n=n_quantiles)
-
-		# for r in range(n_quantiles-1):
-		# 	print(round(r/n_quantiles,3), q_gene[r], q_inter[r], q_all[r], sep='\t')
 
 		# sys.exit()
-
-
 
 
 
