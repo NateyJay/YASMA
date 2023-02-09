473de2b91ac757f40536156694b77e9d2b3acec4
Nate Johnson <jax523@gmail.com>
2023-02-09 18:41:50 -0300

    Update cli.py
    Cleaning up

--- modules/cli.py
+++ modules/cli.py
@@ -34,9 +34,6 @@ class customClickClass(click.Group):
 
 
 
-
-
-
 	def command(self, *args, **kwargs):
 		"""Gather the command help groups"""
 		help_group = kwargs.pop('group', None)
@@ -85,19 +82,6 @@ class customClickClass(click.Group):
 		return super(customClickClass, self).parse_args(ctx, args)
 
 						
-# def __init__(self, *args, **kwargs):
-# 	context_settings = kwargs.setdefault('context_settings', {})
-# 	if 'help_option_names' not in context_settings:
-# 		context_settings['help_option_names'] = ['-h', '--help']
-# 	self.help_flag = context_settings['help_option_names'][0]
-# 	super(DefaultHelp, self).__init__(*args, **kwargs)
-
-# def parse_args(self, ctx, args):
-# 	if not args:
-# 		args = [self.help_flag]
-# 	return super(DefaultHelp, self).parse_args(ctx, args)
-
-
 
 
 
@@ -105,17 +89,6 @@ class customClickClass(click.Group):
 def cli():
 	"""YASA (Yet Another sRNA Annotator)"""
 
-
-# @cli.command(group='Zebra', help_priority=1)
-# def NotReal():
-# 	'''just a fake command'''
-# 	pass
-
-
-# @cli.command(group='Annotation', help_priority=1)
-# def Aardvark():
-# 	'''just a fake command'''
-# 	pass
 
 
 
