45288f791c99e6a308b80ee9434f0407ca1589ed
Nate Johnson <jax523@gmail.com>
2023-08-09 08:50:58 -0400

    Update __init__.py
    Added new tools trim, align

--- yasma/__init__.py
+++ yasma/__init__.py
@@ -1,19 +1,14 @@
-# from .precheck import *
-from .context import *
+
 from .count import *
+from .context import *
 from .hairpin import *
-# from .poisson import *
-# from .dicer import *
 from .readgroups import *
-# from .coverage import *
-# from .target import *
 from .adapter import *
-# from .lambda_cov import *
-# from .annotate import *
 from .peak import *
 from .jbrowse import *
 from .inputs import *
-
+from .trim import *
+from .align import *
 
 
 ## Functionality to add:
