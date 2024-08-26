57a68226487ac7460e7f7c29e089db69a3933408
Nate Johnson <jax523@gmail.com>
2024-08-26 13:05:47 -0400

    Update count.py

--- yasma/count.py
+++ yasma/count.py
@@ -2,6 +2,9 @@
 
 import os
 import click
+from click_option_group import optgroup
+
+
 from subprocess import PIPE, Popen, call
 from pathlib import Path
 from os.path import isfile, isdir
@@ -65,38 +68,50 @@ def init(l, r, a, o, ):
 
 @cli.command(group='Calculation', help_priority=3)
 
-@click.option("-a", "--alignment_file", 
+
+@optgroup.group('\n  Basic options',
+				help='')
+
+@optgroup.option("-a", "--alignment_file", 
 	required=False, 
 	type=click.UNPROCESSED, callback=validate_path,
 	help='Alignment file input (bam or cram).')
 
-@click.option("-o", "--output_directory", 
+@optgroup.option("-o", "--output_directory", 
 	required=False,
 	type=click.Path(),
 	help="Directory name for annotation output")
 
-@click.option("-l", "--locus_files", 
+@optgroup.option("-c", "--conditions", 
+	required=False, 
+	multiple=True,
+	type=click.UNPROCESSED, callback=validate_condition,
+	help='Values denoting condition groups (sets of replicate libraries) for projects with multiple tissues/treatments/genotypes. Can be entered here as space sparated duplexes, with the library base_name and condition groups delimited by a colon. E.g. SRR1111111:WT SRR1111112:WT SRR1111113:mut SRR1111114:mut')
+
+
+@optgroup.option("-a", "--annotation_file", 
 	required=False, 
 	type=click.Path(exists=True),
-	multiple=True,
-	help='list of loci in 2-column format (name, chrom:start-stop). Multiples allowed. Defaults to the results file.')
+	default = Path("tradeoff", "loci.gff3"),
+	multiple=False,
+	help='Locus annotation in gff, gff3, bed, or tabular format. Tabular requires locus_name and contig:start-stop in the first two columns (tab-delimited, "#" escape char). Defaults to tradeoff/loci.gff3, but that may not be prefered if alignment includes conditions.')
 
-@click.option("-g", "--gff_files", 
-	required=False, 
-	type=click.Path(exists=True),
-	multiple=True,
-	help='gff of genes to be evaluated. Looks for "mRNA" features as would be described in an NCBI annotation. Multiples allowed. Defaults to None.')
+@optgroup.option("-n", "--name", 
+	required=False,
+	type=str,
+	help="Optional name. Useful if comparing annotations.")
 
-@click.option("--ignore_zeroes",
-	is_flag=True,
-	help="Don't save zero depth entries to deepcounts.txt. Saves file space, but can make it harder to analyze.")
+
+
+@optgroup.option("--include_zeros",
+	is_flag=False,
+	help="Include to save 0-depth rows in the deep counts. By default, these are excluded to save space (except for one entry to make sure downstream analyses will include un-found entries)")
 
 
 def count(** params):
 	"""Gets counts for all readgroups, loci, strand, and sizes."""
 
 	rc = requirementClass()
-	rc.add_samtools()
 	rc.check()
 
 	ic = inputClass(params)
@@ -104,103 +119,119 @@ def count(** params):
 
 	output_directory     = str(ic.output_directory)
 	alignment_file       = ic.inputs['alignment_file']
+	conditions           = ic.inputs['conditions']
 
-	locus_files          = params['locus_files']
-	gff_files            = params['gff_files']
-	ignore_zeroes        = params['ignore_zeroes']
+	annotation_file      = params['annotation_file']
+	include_zeros        = params['include_zeros']
+	name                 = params['name']
 
+	Path(output_directory, "counts").mkdir(parents=True, exist_ok=True)
 
-	print(gff_files)
+	if name:
+		counts_file      = Path(output_directory, 'counts', f'{name}_counts.txt')
+		deep_counts_file = Path(output_directory, 'counts', f'{name}_deepcounts.txt') 
 
-	Path(output_directory+ "/counts/").mkdir(parents=True, exist_ok=True)
-
-
-	counts_file = f"{output_directory}/counts/counts.txt"
-	deep_counts_file = f"{output_directory}/counts/deepcounts.txt"
-
-	if len(locus_files) == 0:
-		locus_files = [f"{output_directory}/tradeoff/loci.txt"]
+	else:
+		counts_file      = Path(output_directory, 'counts', 'counts.txt')
+		deep_counts_file = Path(output_directory, 'counts', 'deepcounts.txt') 
 
 
 	c = Counter()
 
-	print('annotations:')
-	loci = []
-	for file in locus_files:
-		with open(file, 'r') as f:
-			header = f.readline()
-			for line in f:
-				c[file] += 1
-				line = line.strip().split("\t")[:2]
-				line[1] = line[1].replace("..", "-")
-				loci.append((line[1], line[0]))
 
-	for file in gff_files:
+	def process_annotation(file):
 		with open(file, 'r') as f:
-			for line in f:
+			for i,line in enumerate(f):
 				if not line.startswith("#"):
 					line = line.strip().split("\t")
-					if line[2] == 'gene':
-						c[file] += 1
+
+					if file.suffix == ".txt":
+						name, coords = line[:2]
+						coords = coords.replace("..", '-')
+
+					elif file.suffix == '.gff' or file.suffix == '.gff3':
 						coords = f"{line[0]}:{line[3]}-{line[4]}"
-						name = line[8].split(";")[0].split("=")[1]
+						name   = line[8].split(";")[0].split("=")[-1]
 
-						loci.append((name, coords))
+					elif file.suffix == '.bed':
+						name   = f"bed_{i}"
+						coords = f"{line[0]}:{line[1]}-{line[2]}"
 
-	for key, val in c.items():
-		print(" ", key, "->", val, 'loci')
+					else:
+						print(f'file.suffix "{file.suffix}" not expected in annotation file...')
+						sys.exit()
+
+					yield (name, coords)
+
+
+
+
+	print('annotations:')
+
+	loci = []
+
+
+	for name, coords in process_annotation(annotation_file):
+		# c[file] += 1
+		loci.append((name, coords))
+
+
+
+	# for file in locus_files:
+	# 	with open(file, 'r') as f:
+	# 		header = f.readline()
+	# 		for line in f:
+	# 			c[file] += 1
+	# 			line = line.strip().split("\t")[:2]
+	# 			line[1] = line[1].replace("..", "-")
+	# 			loci.append((line[1], line[0]))
+
+	# for file in gff_files:
+	# 	with open(file, 'r') as f:
+	# 		for line in f:
+	# 			if not line.startswith("#"):
+	# 				line = line.strip().split("\t")
+	# 				if line[2] == 'gene':
+	# 					c[file] += 1
+	# 					coords = f"{line[0]}:{line[3]}-{line[4]}"
+	# 					name = line[8].split(";")[0].split("=")[1]
+
+	# 					loci.append((name, coords))
+
+	# for key, val in c.items():
+	# 	print(" ", key, "->", val, 'loci')
 
 	print('')
-	print('processing annotations...')
+	print('processing annotation...')
 
 
 	chroms, rgs = get_chromosomes(alignment_file)
 
-	# print('  -> making empty genome structure')
-	# lookup = {}
-	# for c,l in chroms:
-	# 	lookup[c] = [None] * l
-	# 	# lookup[c] = {}
+	pprint(conditions)
+
+	rev_conditions = {}
+	for k,vs in conditions.items():
+		for v in vs:
+			rev_conditions[v] = k
 
 
-	# print('  -> populating')
-	# pbar = tqdm(total = len(loci), ncols=90)
-	# for name, locus in loci:
-	# 	pbar.update()
-	# 	# print(locus)
-
-	# 	chrom, start, stop = parse_locus(locus)
-	# 	# print(chrom, start, stop)
-
-	# 	for r in range(start, stop + 1):
-	# 		try:
-	# 			lookup[chrom][r].add(name)
-	# 		except:
-	# 			lookup[chrom][r] = set([name])
-	# pbar.close()
-
-
-
-
-	# depth_c = get_rg_depth(output_directory, alignment_file)
-
-	# print(sum(depth_c.values()))
-	# pprint(depth_c)
-	# sys.exit()
-
+	cond_rgs = [f"{rev_conditions[r]}.{r}" for r in rgs]
 
 	with open(counts_file, 'w') as outf:
-		print('name','locus', "\t".join(rgs), sep='\t', file=outf)
+		print('name','locus', "\t".join(cond_rgs), sep='\t', file=outf)
 
 	with open(deep_counts_file, 'w') as outf:
-		print('name','rg','length','strand','count', sep='\t', file=outf)
+		print('name', 'condition', 'rg','length','strand','count', sep='\t', file=outf)
 
 
-
+	missed_rg     = set(rgs)
+	missed_length = set(range(15,31))
+	missed_strand = set(["-","+"])
 
 	perc = percentageClass(1,len(loci))
 
 	for i, locus in enumerate(loci):
+
 		name, locus = locus
 
 		c = Counter()
@@ -220,6 +251,7 @@ def count(** params):
 			deep_c[(sam_rg, sam_length, sam_strand)] += 1
 
 
+
 		line = [name, locus]
 
 		line += [c[rg] for rg in rgs]
@@ -230,14 +262,48 @@ def count(** params):
 
 		with open(deep_counts_file, 'a') as outf:
 			for rg in rgs:
+				try:
+					cond = rev_conditions[rg]
+				except KeyError:
+					cond = 'None'
+
 				for length in range(15,31):
 					for strand in {"+", "-"}:
 						count = deep_c[(rg, length, strand)]
 
-						if count == 0 and ignore_zeroes:
-							pass
-						else:
-							print(name, rg, length, strand, count, sep='\t', file=outf)
+						if count > 0 or include_zeros:
+							print(name, cond, rg, length, strand, count, sep='\t', file=outf)
+
+							try:
+								missed_strand.remove(strand)
+							except KeyError:
+								pass
+
+							try:
+								missed_rg.remove(rg)
+							except KeyError:
+								pass
+	
+							try:
+								missed_length.remove(length)
+							except KeyError:
+								pass
+						
+					
+
+	with open(deep_counts_file, 'a') as outf:
+		for rg in missed_rg:
+			cond = rev_conditions[rg]
+			print(name, cond, rg, '15', '-', '0', sep='\t', file=outf)
+		for strand in missed_strand:
+			cond = rev_conditions[rg]
+			print(name, cond, rg, '15', strand, '0', sep='\t', file=outf)
+		for length in missed_length:
+			print(name, cond, rg, length, '-', '0', sep='\t', file=outf)
+
+
+
+
 
 
 	print()
