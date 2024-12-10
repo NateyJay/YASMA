79baf8cd5bbfdd7c0a715a6bb9cd392eb0cbd53c
Nate Johnson <jax523@gmail.com>
2024-12-10 16:36:27 -0300

    More cleanup
    Some stragglers.. More to come i'm sure.

--- yasma/__init__.py
+++ yasma/__init__.py
@@ -2,30 +2,21 @@ from .tradeoff import *
 from .count import *
 from .context import *
 from .hairpin import *
-from .readgroups import *
+# from .readgroups import *
 from .adapter import *
 from .jbrowse import *
 from .inputs import *
 from .trim import *
-from .align import *
+# from .align import *
 from .other import *
 from .size_profile import *
 from .shortstack3 import *
 from .shortstack4 import *
-from .subsample import *
+# from .subsample import *
 from .nativealign import *
 from .download import *
 from .coverage import *
 from .merge import *
-# from .jenks import *
 
-
-## Functionality to add:
-## [x] Requirements checking
-## [ ] Documentation on github
-## [ ] Logging of all functions
-## [x] Sensible output naming
-## [ ] Alignment convenience function
-## [x] Checking that alignment/genome/annotations all match in chromosome names.
 
 
