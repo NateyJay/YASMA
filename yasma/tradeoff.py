32e84e67fac7b820cb5afd82b1fcb4a19a5ddc02
Nate Johnson <jax523@gmail.com>
2024-12-20 15:14:37 -0300

    Update tradeoff.py
    Changed error to warning... This might not be fatal if the libraries are very shallow (though there will likely be other problems)

--- yasma/tradeoff.py
+++ yasma/tradeoff.py
@@ -1374,7 +1374,7 @@ def tradeoff(**params):
 
 
 	if threshold_stats['threshold'] == 0.0:
-		sys.exit("Error: detected threshold for annotation is 0 reads per million (0 reads).\nThis will annotate 100%% of reads, leading to a highly unrepresentative sample. This might be caused by problems with the alignment, libraries (check these), or an internal problem with YASMA (please make an issue on github)")
+		print("Warning: detected threshold for annotation is 0 reads per million (0 reads).\nThis will annotate 100%% of reads, leading to a highly unrepresentative sample. This might be caused by problems with the alignment (possibly low absolute alignment), libraries (check file paths in inputs.json), or an internal problem with YASMA (please make an issue on github or report to Nate)\n")
 
 
 
