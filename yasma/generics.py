f304d63dc02266849cf2ba3345b03eec31c66d84
Nate Johnson <jax523@gmail.com>
2025-01-24 12:30:09 -0300

    Fixed project_name bug

--- yasma/generics.py
+++ yasma/generics.py
@@ -763,13 +763,15 @@ def validate_outdir(ctx, param, od):
 		if not Path(od, 'inputs.json').is_file():
 			sys.exit("Error: cannot run yasma in an uninitialized directiory (doesn't contain inputs.json) without specifying -o/--output_directory.\n\nTo initialize as a yasma directory, run the same command including the directory you want to use as the home directory of the analysis (-o ./path/to/my_directory) or the current directory with (-o .). This directory name will be used as the project name.")
 
+
+
 	else:
 		od = Path(od)
 		if not od.is_dir():
 			sys.exit(f"InputError: --output_directory '{od}' not found!")
 
-
-	# name = od.name
+	if od == Path("."):
+		od = Path().cwd()
 
 	return(od)
 
